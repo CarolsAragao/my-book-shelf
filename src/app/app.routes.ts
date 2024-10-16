687989ee755e0228a0febce39af24ef6a344a9a1
CarolsAragao <carolinesousaa96@gmail.com>
2024-10-16 19:52:51 -0300

    banner

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { HomeComponent } from './home/home.component';
 import { NavComponent } from './nav/nav.component';
 
 export const routes: Routes = [
-{path: 'home', component: HomeComponent},
-{path: '', component: NavComponent}
+{path: '', component: HomeComponent},
+{path: 'nav', component: NavComponent}
 ];
