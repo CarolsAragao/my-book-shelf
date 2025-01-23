eb1e5272236ab285637a7754d8d74557dc7992c4
Carol <carolinesousaa96@gmail.com>
2025-01-22 22:11:55 -0300

    environment changes

--- src/app/core/services/base.service.ts
+++ src/app/core/services/base.service.ts
@@ -1,13 +1,14 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
+import { environment } from "../../../environments/environment.development";
 
 @Injectable({
     providedIn: 'root'
 })
 
 export class BaseService {
-	apiUrl = 'https://localhost:7052/api';
+	apiUrl = environment.apiUrl;
 
 	constructor(public http: HttpClient) {}
   get<T>(controller: string): Observable<T> {
