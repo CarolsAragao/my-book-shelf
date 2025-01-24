3cd9b8350c20a0c1bf96e3adb104e9725423796b
Carol <carolinesousaa96@gmail.com>
2025-01-23 21:36:54 -0300

    creating add new user form component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { MenuComponent } from './features/menu/menu.component';
 import { LoginComponent } from './features/login/login.component';
 import { NavComponent } from './features/nav/nav.component';
 import { authGuard } from './core/guard/auth.guard';
+import { UserFormComponent } from './features/user-form/user-form.component';
 
 export const routes: Routes = [
 {path: 'home', component: HomeComponent, canActivate: [authGuard]},
 {path: 'nav', component: NavComponent, canActivate: [authGuard]},
 {path: 'menu', component: MenuComponent, canActivate: [authGuard]},
-{path: '', component: LoginComponent}
+{path: '', component: LoginComponent},
+{path: 'addUser', component: UserFormComponent}
 ];
