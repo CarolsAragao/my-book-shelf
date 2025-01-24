0b85fe40d29cb13ef0ba65745fb3f06fadfe5cac
Carol <carolinesousaa96@gmail.com>
2025-01-23 21:37:11 -0300

    adjusting menu component style

--- src/app/features/menu/menu.component.ts
+++ src/app/features/menu/menu.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MegaMenuModule } from 'primeng/megamenu';
 import { MegaMenuItem } from 'primeng/api';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -12,20 +13,36 @@ import { MegaMenuItem } from 'primeng/api';
 export class MenuComponent implements OnInit{
   items: MegaMenuItem[] | undefined;
 
-  ngOnInit() {
-    this.items = [
+  constructor(private _router: Router) {}
+ 
+    ngOnInit() {
+      this.items = [
         {
-            icon: 'pi pi-bars',
+            label: 'User',
             items: [
                 [
                     {
-                        label: 'My Shelf'
-                        // items: [{ label: 'Accessories' }]
-                    }
-                ]                  
-           
+                        items: [
+                            { label: 'Add User', command: () => this._router.navigate(['addUser']) }                          
+                        ],
+                    },
+                ]         
+            
             ]
-        }     
-    ]    
+        },
+        {
+            label: 'Books',
+            items: [
+                [
+                    {
+                        items: [
+                            { label: 'My Books' }
+                          
+                        ],
+                    }
+                ]              
+            ]
+        }      
+    ];
   }
 }
